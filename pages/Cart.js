9b44795ffaf2c6f2d6fcdeb17d380b224d73d76d
PrajapatiVikrant <146848720+PrajapatiVikrant@users.noreply.github.com>
2024-11-07 18:43:41 +0530

    Ecomerce

--- pages/Cart.js
+++ pages/Cart.js
@@ -45,14 +45,14 @@ const Cart = ()=>{
          order_id:data.id,
          image:'Logo',
          handler:async function(response){
-          let res = await axios.post('https://expert-carnival-ww9776xrgqpfg6wj-3000.app.github.dev/api/order/verify',{response},{
+          let res = await axios.post('https://ecomerce-backend-mauve.vercel.app/api/order/verify',{response},{
             headers:{
               Authorization: `Bearer ${localStorage.getItem('token')}`, 
             }
           } )
           if(res.data === 'sign valid')
            console.log(res.data)
-            await axios.post('https://expert-carnival-ww9776xrgqpfg6wj-3000.app.github.dev/api/order',{data:cartList,total_price:total},{
+            await axios.post('https://ecomerce-backend-mauve.vercel.app/api/order',{data:cartList,total_price:total},{
                 headers:{
                   Authorization: `Bearer ${localStorage.getItem('token')}`, 
                 }
@@ -73,7 +73,7 @@ const Cart = ()=>{
 async function orderNow(){
      
 
-    const orderRes = await axios.post(`https://expert-carnival-ww9776xrgqpfg6wj-3000.app.github.dev/api/order/order`,{amount:total},{
+    const orderRes = await axios.post(`https://ecomerce-backend-mauve.vercel.app/api/order/order`,{amount:total},{
         headers:{
           Authorization: `Bearer ${localStorage.getItem('token')}`, 
         }
