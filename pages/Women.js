9f4aefab24f0f06e3918deb5b62fc5be0bd20843
PrajapatiVikrant <146848720+PrajapatiVikrant@users.noreply.github.com>
2024-11-07 18:04:07 +0530

    Ecomerce

--- pages/Women.js
+++ pages/Women.js
@@ -38,10 +38,10 @@ const Women = ()=>{
                  <Filter list = {filterlist} category = {productData.category}/>
                 </section>
                 <section className="pl-14 grid grid-cols-1 md:grid-cols-3 gap-24">
-                   {/* {productData.watchCategory.map((elem)=>{
+                   {productData.watchCategory.map((elem)=>{
                  
                     return <WatchCategory categoryList = {elem.items}/>
-                   })} */}
+                   })}
                   
                    
                   
