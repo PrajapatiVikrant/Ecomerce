50b5cae5a7a3942e5783968d06dc6255bdb94bff
PrajapatiVikrant <146848720+PrajapatiVikrant@users.noreply.github.com>
2024-12-03 21:44:37 +0530

    watch store

--- components/Category.js
+++ components/Category.js
@@ -7,15 +7,17 @@ const Category = ({name,list}) => {
     return (
         <div className="text-white">
             <h2 className="text-4xl m-7 text-center font-bold">{name}</h2>
-            <ul className="md:flex overflow-auto border-4 justify-center items-center">
+            <ul className="md:flex overflow-auto  justify-center items-center">
                 {list.map((elem)=>{
                     return (
-                        <li className="m-5 cursor-pointer">
+                        <li className="m-5 border-4 cursor-pointer">
+                            <center>
                         <img 
                         style={{height:"200px",width:"350px"}}
                         src={elem.url} 
                         alt="Image"
                          />
+                         </center>
                         <p className="text-center">{elem.name}</p>
                     </li>
                     )
