5cd54536904c525972056ffd7c9c45d621d1ec0e
PrajapatiVikrant <146848720+PrajapatiVikrant@users.noreply.github.com>
2025-10-24 15:55:00 +0530

    change navbar logic

--- components/Navbar.js
+++ components/Navbar.js
@@ -1,10 +1,9 @@
 import { FaClock } from "react-icons/fa6";
 import { VscAccount } from "react-icons/vsc";
-import react, { useEffect } from "react";
+import React, { useEffect, useState } from "react";
 import { CiShoppingCart } from "react-icons/ci";
 import Link from "next/link";
 import axios from "axios";
-
 import { useDispatch, useSelector } from "react-redux";
 import { ChangeProductlist } from "../state/Slice/ProductsDisplaySlice";
 import { getCartNo } from "../state/Slice/CartCountSlice";
@@ -12,81 +11,104 @@ import { ChangeName } from "../state/Slice/NameSlice";
 import { changeCartlist } from "../state/Slice/CartSlice";
 import { useRouter } from "next/navigation";
 
-
-const Navbar = ()=>{
-  
-  const counter = useSelector((state)=>{
-    return state.counter
-  })
-  const name = useSelector((state)=>{
-    return state.name;
-  })
+const Navbar = () => {
+  const counter = useSelector((state) => state.counter);
+  const name = useSelector((state) => state.name);
   const dispatch = useDispatch();
   const router = useRouter();
 
+  const [isLoggedIn, setIsLoggedIn] = useState(false);
 
-  useEffect(()=>{
-    getcartItem()
- },[])
+  useEffect(() => {
+    getCartItem();
+  }, []);
 
-async function getcartItem(){
-  const data = await axios.get('https://ecomerce-backend-two.vercel.app/api/cart',{
-    headers:{
-      Authorization: `Bearer ${localStorage.getItem('token')}`, 
+  async function getCartItem() {
+    const token = localStorage.getItem("token");
+    if (!token) {
+      setIsLoggedIn(false);
+      return;
     }
-  } )
-  console.log(data.data)
-  const isLogin = await axios.get('https://ecomerce-backend-two.vercel.app/api/auth',{
-    headers:{
-      Authorization: `Bearer ${localStorage.getItem('token')}`, 
+
+    try {
+      const data = await axios.get("https://ecomerce-backend-two.vercel.app/api/cart", {
+        headers: { Authorization: `Bearer ${token}` },
+      });
+
+      const isLogin = await axios.get("https://ecomerce-backend-two.vercel.app/api/auth", {
+        headers: { Authorization: `Bearer ${token}` },
+      });
+
+      if (isLogin.data.name) {
+        dispatch(ChangeName(isLogin.data.name));
+        setIsLoggedIn(true);
+      }
+
+      if (data.data !== "Invalid Token") {
+        dispatch(getCartNo(data.data.length));
+        dispatch(changeCartlist(data.data));
+      }
+    } catch (err) {
+      console.log("Error fetching user/cart:", err);
+      setIsLoggedIn(false);
     }
-  } )
- 
-  if(isLogin.data.name){
-    dispatch(ChangeName(isLogin.data.name))
   }
-  console.log(data.data)
-  if(data.data != 'Invalid Token'){
-   
-    dispatch(getCartNo(data.data.length));
-    dispatch(changeCartlist(data.data))
-  }
- 
-}
 
+  async function menclick() {
+    const data = await axios.get("https://ecomerce-backend-two.vercel.app/api/product/Men");
+    dispatch(ChangeProductlist(data.data[0]));
+  }
 
+  async function womenclick() {
+    const data = await axios.get("https://ecomerce-backend-two.vercel.app/api/product/Women");
+    dispatch(ChangeProductlist(data.data[0]));
+  }
 
-  async function menclick(){
-    const data = await axios.get('https://ecomerce-backend-two.vercel.app/api/product/Men');
-   
-    dispatch(ChangeProductlist(data.data[0]))
+  async function kidsclick() {
+    const data = await axios.get("https://ecomerce-backend-two.vercel.app/api/product/Kids");
+    dispatch(ChangeProductlist(data.data[0]));
   }
-  async function womenclick(){
-    const data = await axios.get('https://ecomerce-backend-two.vercel.app/api/product/Women');
- 
-    dispatch(ChangeProductlist(data.data[0]))
-  }
-  async function kidsclick(){
-    const data = await axios.get('https://ecomerce-backend-two.vercel.app/api/product/Kids');
-    console.log(data)
-    dispatch(ChangeProductlist(data.data[0]))
-  }
-    return (
-        <nav className="flex justify-around text-white py-4 items-center">
-       <Link href="/"> <FaClock className="cursor-pointer mt-1 text-3xl"/></Link>
-        <ul className="flex w-1/2 justify-around md:text-2xl">
-          <Link href="/Men" className="cursor-pointer hover:underline  hover:text-green-600" onClick={menclick}>Men</Link>
-          <Link href="/Women" className="cursor-pointer hover:underline  hover:text-green-600" onClick={womenclick}>Women</Link>
-          <Link href="/Kids" className="cursor-pointer hover:underline  hover:text-green-600" onClick={kidsclick}>Kids</Link>
-          
-          
-        </ul>
-       <ul className="flex justify-around  w-1/6 md:text-3xl">
-        <Link href="/Login" className="cursor-pointer flex flex-col mx-2 justify-center items-center"><VscAccount/><div className="text-xs">{name}</div></Link>
-        <Link href="/Cart" className="flex items-center"><CiShoppingCart/><div className="text-[5px] md:text-xs relative right-3.5 md:right-9 bottom-3 bg-red-500 text-white p-1 px-2 rounded-full">{counter}</div></Link>
-       </ul>
-        </nav>
-    )
-}
+
+  return (
+    <nav className="flex justify-around text-white py-4 items-center">
+      <Link href="/">
+        <FaClock className="cursor-pointer mt-1 text-3xl" />
+      </Link>
+
+      <ul className="flex w-1/2 justify-around md:text-2xl">
+        <Link href="/Men" className="cursor-pointer hover:underline hover:text-green-600" onClick={menclick}>
+          Men
+        </Link>
+        <Link href="/Women" className="cursor-pointer hover:underline hover:text-green-600" onClick={womenclick}>
+          Women
+        </Link>
+        <Link href="/Kids" className="cursor-pointer hover:underline hover:text-green-600" onClick={kidsclick}>
+          Kids
+        </Link>
+      </ul>
+
+      <ul className="flex justify-around w-1/6 md:text-3xl">
+        {isLoggedIn ? (
+          <>
+            <Link href="/Login" className="cursor-pointer flex flex-col mx-2 justify-center items-center">
+              <VscAccount />
+              <div className="text-xs">{name}</div>
+            </Link>
+            <Link href="/Cart" className="flex items-center relative">
+              <CiShoppingCart />
+              <div className="text-[5px] md:text-xs absolute right-[-12px] bottom-3 bg-red-500 text-white p-1 px-2 rounded-full">
+                {counter}
+              </div>
+            </Link>
+          </>
+        ) : (
+          <Link href="/Login" className="cursor-pointer text-lg bg-green-600 hover:bg-green-700 px-4 py-1 rounded-md">
+            Sign In
+          </Link>
+        )}
+      </ul>
+    </nav>
+  );
+};
 
 export default Navbar;
